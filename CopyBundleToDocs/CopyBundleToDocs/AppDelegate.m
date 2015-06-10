072db3b5a3c617fd9e48888ed93f3e0ab20cb673
Shailesh Kumar <shailesh.kumar@outlook.com>
2015-06-10 22:21:19 +0200

    Added CopyBundleToDocs example

---
+++ CopyBundleToDocs/CopyBundleToDocs/AppDelegate.m
@@ -0,0 +1,71 @@
+//
+//  AppDelegate.m
+//  CopyBundleToDocs
+//
+//  Created by shailesh on 10/06/15.
+//  Copyright (c) 2015 shailesh. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (NSString*) getPathForDirectory:(int)directory {
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(directory, NSUserDomainMask, YES);
+    return [paths objectAtIndex:0];
+}
+
+- (void)copyFromBundleToDocuments {
+    NSString *appBundleDirectory = [[NSBundle mainBundle] resourcePath];
+    NSString* documentsDirectory = [self getPathForDirectory:NSDocumentDirectory];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSError *error =  nil;
+    NSArray* itemsToCopy = [fileManager contentsOfDirectoryAtPath:appBundleDirectory error:&error];
+    if(error){
+        NSLog(@"Failed to get contents of App Bundle");
+        return;
+    }
+    for (NSString* item in itemsToCopy) {
+        NSString *sourceDir = [appBundleDirectory stringByAppendingFormat:@"/%@",item];
+        NSString *destDir = [documentsDirectory stringByAppendingFormat:@"/%@",item];
+        BOOL copySuccess =  [fileManager copyItemAtPath:sourceDir toPath:destDir error:&error];
+        if (!copySuccess) {
+            NSLog(@"copy failed");
+        }
+    }
+}
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    [self copyFromBundleToDocuments];
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+@end
---
+++ CopyBundleToDocs/CopyBundleToDocs/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CopyBundleToDocs
+//
+//  Created by shailesh on 10/06/15.
+//  Copyright (c) 2015 shailesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
